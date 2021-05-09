bd3a61c6f8ab27a36162af0409c2cad05e12f6cf
AngelCarrasquedo <83425882+AngelCarrasquedo@users.noreply.github.com>
2021-05-09 06:13:28 -0500

    Add files via upload

---
+++ practica_3.ino
@@ -0,0 +1,23 @@
+int led = 13;
+char leer;
+boolean prendido=false;
+
+void setup() {
+Serial.begin(9600);
+pinMode(led, OUTPUT);
+}
+
+void loop() {
+leer =Serial.read();
+
+if((leer== 'a') && (prendido== false))
+{
+digitalWrite(led,HIGH);
+prendido=true;
+}
+else if((leer== 'a') && (prendido== true))
+{
+digitalWrite(led,LOW);
+prendido = false;
+}
+}
